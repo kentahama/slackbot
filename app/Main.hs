0cd8dc935919f308c735fcc75601ff1b95874a62
Kenta Hamanaka <hamanaka@cs.info.mie-u.ac.jp>
2017-02-06 03:14:18 +0900

    Add sample code

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import Web.Slack
+import Web.Slack.Message
+import System.Environment (lookupEnv)
+import Data.Maybe (fromMaybe)
+import Control.Applicative
+
+myConfig :: String -> SlackConfig
+myConfig apiToken = SlackConfig
+         { _slackApiToken = apiToken -- Specify your API token here
+         }
+
+echoBot :: SlackBot ()
+echoBot (Message cid _ msg _ _ _) = sendMessage cid msg
+echoBot _ = return ()
 
 main :: IO ()
-main = someFunc
+main = do
+  apiToken <- fromMaybe (error "SLACK_API_TOKEN not set")
+               <$> lookupEnv "SLACK_API_TOKEN"
+  runBot (myConfig apiToken) echoBot ()
