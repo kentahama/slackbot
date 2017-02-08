cc82330b9eee70c73b8d00826e0a3765d2303949
Kenta Hamanaka <hamanaka@cs.info.mie-u.ac.jp>
2017-02-08 12:08:20 +0900

    Implement myBot Squashed commit of the following:
    commit 6d2f2643e6a6b92698125cc534f637bc86e30fb0
    Author: Kenta Hamanaka <hamanaka@cs.info.mie-u.ac.jp>
    Date:   Wed Feb 8 11:32:08 2017 +0900
    
        ファイル分けた
    
    commit 564161bfaae38532bd01fc031d52186dc87c6341
    Author: Kenta Hamanaka <hamanaka@cs.info.mie-u.ac.jp>
    Date:   Wed Feb 8 05:25:53 2017 +0900
    
        動いた

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,27 @@
 module Lib
-    ( someFunc
+    ( myBot
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Web.Slack
+import Web.Slack.Message
+
+import Control.Lens
+
+import Control.Monad
+import Control.Monad.IO.Class
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+idText :: UserId -> T.Text
+idText uid = T.pack $ "<" ++ show uid ++ ">"
+
+myBot :: SlackBot ()
+myBot Hello = do
+  myid <- use $ session . slackSelf . selfUserId . getId
+  liftIO $ print myid
+myBot (Message cid who msg time styp edt) = do
+  myid <- use $ session . slackSelf . selfUserId . getId
+  -- when (idText myid `T.isInfixOf` msg) $ sendMessage cid "Hi!"
+  liftIO $ TIO.putStr msg
+myBot _     = return ()
